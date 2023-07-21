27fefb9e8a326a9986de4134734d19d6ac037e90
Justin Miller <jrmill94@icloud.com>
2023-07-21 11:19:45 -0500

    added dynamically driven content

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,27 @@
 const container = document.querySelector('#container')
+import { getPikomon } from "./database.js";
+
+// Grab the database
+// Converting database to HTML string
+// set string to variable
+// return variable
+// put it on website
+
+const pikomon = getPikomon()
+
+console.log(pikomon)
+
+let pikoCards = ``
+
+for (const pikoman of pikomon) {
+    pikoCards += `<section class="piko-card">
+    <img class="piko-img" src="${pikoman.imageUrl}"/>
+    <h3>${pikoman.name}</h3>
+    <p class="piko-info">Category:${pikoman.category}</p>
+    <p class="piko-info">Abilities:${pikoman.abilities}</p>
+    <p class="piko-info">Weakness:${pikoman.weakness}</p> 
+</section>`
+}
+
+container.innerHTML = pikoCards
+
